e171d083dffc5fd1c399557652fca9c20e51eafd
Touch <s.chantouch@mango-byte.com>
2019-01-11 02:18:59 +0700

    add keydown event and clear form

--- Errors.js
+++ Errors.js
@@ -1,36 +1,66 @@
 class Errors {
 
-    constructor(){
-        this.errors = {};
-    }
+  constructor () {
+    this.errors = {}
+  }
 
-    has(key){
-        return this.errors[key] !== undefined
-    }
+  has (key) {
+    return this.errors[key] !== undefined
+  }
 
-    first(key){
-        if(this.has(key)){
-            return this.errors[key][0]
+  first (key) {
+    if (this.has(key)) {
+      return this.errors[key][0]
+    }
+  }
+
+  get (key) {
+    if (this.has(key)) {
+      return this.errors[key]
+    }
+  }
+
+  all () {
+    return this.errors
+  }
+
+  fill (values) {
+    this.errors = values
+  }
+
+  flush () {
+    this.errors = {}
+  }
+
+  /**
+   * Clear one or all error fields.
+   *
+   * @param {String|undefined} field
+   */
+  clear (field) {
+    const errors = {}
+
+    if (field) {
+      Object.keys(this.errors).forEach(key => {
+        if (key !== field) {
+          errors[key] = this.errors[key]
         }
+      })
     }
 
-    get(key){
-        if(this.has(key)){
-            return this.errors[key]
-        }
-    }
+    this.fill(errors)
+  }
 
-    all(){
-        return this.errors
+  /**
+   * Clear errors on keydown.
+   *
+   * @param {KeyboardEvent} event
+   */
+  onKeydown (event) {
+    if (event.target.name) {
+      this.clear(event.target.name)
     }
-
-    fill(values){
-        this.errors = values
-    }
-
-    flush() {
-        this.errors = {};
-    }
+  }
 
 }
 
--- index.js
+++ index.js
@@ -3,51 +3,51 @@ import ErrorComponent from './ErrorComponent.vue'
 import axios from 'axios'
 
 
-class Validator{
+class Validator {
 
-    install(Vue){
+  install (Vue) {
 
-        Vue.component('error', ErrorComponent);
+    Vue.component('error', ErrorComponent);
 
-        if(Vue.http){
-            Vue.http.interceptors.push((request, next) => {
-                next(response => {
-                    if(response.status === 422){
-                        Errors.fill(response.body)
-                    }
-                });
-            });
-        }
-
-        if (axios) {
-            axios.interceptors.response.use((response) => {
-                return response;
-            }, (error) => {
-                if (error.response.status === 422) {
-                    Errors.fill(error.response.data.errors)
-                }
-
-                return Promise.reject(error);
-            });
-        }
-
-        Vue.mixin({
-
-            beforeCreate(){
-                //errors
-                this.$options.$errors = {};
-                Vue.util.defineReactive(this.$options, '$errors', Errors);
-                if(!this.$options.computed){
-                    this.$options.computed = {}
-                }
-                this.$options.computed["$errors"] = function() {
-                    return this.$options.$errors;
-                };
-            },
-
-        })
-
+    if (Vue.http) {
+      Vue.http.interceptors.push((request, next) => {
+        next(response => {
+          if (response.status === 422) {
+            Errors.fill(response.body)
+          }
+        });
+      });
     }
+
+    if (axios) {
+      axios.interceptors.response.use((response) => {
+        return response;
+      }, (error) => {
+        if (error.response.status === 422) {
+          Errors.fill(error.response.data.errors)
+        }
+
+        return Promise.reject(error);
+      });
+    }
+
+    Vue.mixin({
+
+      beforeCreate () {
+        //errors
+        this.$options.$errors = {};
+        Vue.util.defineReactive(this.$options, '$errors', Errors);
+        if (!this.$options.computed) {
+          this.$options.computed = {}
+        }
+        this.$options.computed["$errors"] = function () {
+          return this.$options.$errors;
+        };
+      },
+
+    })
+
+  }
 
 }
 
